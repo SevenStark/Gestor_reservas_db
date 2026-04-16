25b5453dfab0f62d4293538ee966096fbdfc0c4e
Oscar-R7 <oscarroa835@gmail.com>
2026-04-15 20:56:04 -0500

    guardando app.py

--- app.py
+++ app.py
@@ -124,6 +124,8 @@ def login_huesped():
     else:
         flash("Datos incorrectos, verifica tu correo y cédula.")
         return redirect(url_for('index'))
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
