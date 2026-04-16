a83f78a9d66e50428164fd3d8e8a372a57ae9127
sevenstark <7s.nation1@gmail.com>
2026-04-16 13:14:16 -0500

    guardando cambios

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = '123456'
 
 # Configuración MySQL
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'gestor_reservas'
